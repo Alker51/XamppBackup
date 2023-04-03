bcb545eecec7bfeccccd19349ada864c809186a7
Robin Rémy <remyrobin51@gmail.com>
2023-04-03 20:15:46 +0200

    MODIF :  Header + Folder check/creation

--- XamppBackup.bat
+++ XamppBackup.bat
@@ -1,7 +1,21 @@
 :: Script Settings
 @Echo off
 Setlocal Enabledelayedexpansion
-echo XAMPP Backup
+:: Header of the script
+echo Welcome to XAMPP Backup
+echo This script will create a backup of all your Xampp.
+echo In future features, you will get more features to use this script.
+echo Imagined by Alker / Coded and Created by Alker -- 2023
+Pause
+:: Path of the backup folder
+set dir=C:\xamppBackupStorage
+:: Check if backup folder exists, if not create it
+if not exist C:\xamppBackupStorage\ (
+    mkdir C:\xamppBackupStorage
+        echo Folder '!dir!' created.
+)
+
+echo Your backup will be save in !dir!
 Pause
 echo Shutdown...
 exit
